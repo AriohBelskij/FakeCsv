2e4ed4451bd8ebf179537c5b6cb4466e521040c2
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-12-29 15:54:01 +0200

    make code clean and pretty

--- fakedata/views.py
+++ fakedata/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.db import transaction
 from django.http import JsonResponse, HttpResponseRedirect
